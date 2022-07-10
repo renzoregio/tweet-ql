17afe6afed5e6471725f6aa3380fc658d5941db0
renzoregio <renzorregio@gmail.com>
2022-07-10 10:23:05 +0800

    added query and mutation resolvers

--- server.js
+++ server.js
@@ -1,29 +1,71 @@
 import { ApolloServer, gql } from "apollo-server"
 
+
+const tweets = [
+    {
+        id: "1",
+        text: "First Tweet"
+    },
+    {
+        id: "2",
+        text: "Second Tweet"
+    }
+]
+
 const typeDefs = gql`
     type User {
-        id: ID
-        username: String
+        id: ID!
+        username: String!
+        firstName: String!
+        lastName: String!
     }
 
     type Tweet {
-        id: ID
-        text: String
+        id: ID!
+        text: String!
         author: User
     }
 
     type Query {
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
     }
 
     type Mutation {
-        postTweet(text: String, userId: ID): Tweet
-        deleteTweet(id: ID): Boolean
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
     }
 `
 
-const server = new ApolloServer({ typeDefs })
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(_, { id }) {
+            return tweets.find(tweet => tweet.id === id)
+        }
+    },
+    Mutation: {
+        postTweet(_, { text, userId }) {
+            const tweet = { id: (tweets.length + 1).toString(), text }
+            tweets.push(tweet)
+            return tweet
+        },
+        deleteTweet(_, { id }) {
+            for (let i = 0; i < tweets.length; i++) {
+                if (tweets[i].id === id) {
+                    tweets.splice(i, 1)
+                    return true
+                }
+            }
+
+            return false
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
     console.log(`Running on: ${url}`)
 })
